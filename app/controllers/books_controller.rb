6f525d161a5daf19ff89c20411dcffd141694d08
tenmasato <narito1015@icloud.com>
2021-11-22 13:23:08 +0000

    [Add]削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,12 @@ class BooksController < ApplicationController
     redirect_to book_path(@book.id)
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   def create
     @book = Book.new(book_params)
     @book.save
