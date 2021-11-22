944a5b9d4064e0ccf7d02811bc6ce286bb39124d
tenmasato <narito1015@icloud.com>
2021-11-22 13:16:51 +0000

    [Add]編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   def create
     @book = Book.new(book_params)
     @book.save
