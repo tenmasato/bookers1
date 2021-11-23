5a368be3122806a9888d8489c9a334c8f473e229
tenmasato <narito1015@icloud.com>
2021-11-23 05:32:24 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book.id)
+    else
+    render :show
+    end
   end
 
   def destroy
@@ -27,8 +31,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   private
